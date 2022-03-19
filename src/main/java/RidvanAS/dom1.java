14c74b769bb929ac4ea9b093149164a03d506f7b
RidvanAS <ridvan_07@abv.bg>
2022-03-19 20:49:15 +0200

    domasna1

---
+++ src/main/java/RidvanAS/dom1.java
@@ -0,0 +1,24 @@
+package RidvanAS;
+
+import java.util.Scanner;
+
+public class dom1 {
+    public static void main(String[] args) {
+        System.out.println("Enter your first names");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+
+        System.out.println("Your name is: " + firstName);
+
+        System.out.println("Enter your hobby");
+
+        Scanner Scanner = new Scanner(System.in);
+
+        String userHobby = scanner.nextLine();
+
+        System.out.println("Your hobby is: " + userHobby);
+
+    }
+}
